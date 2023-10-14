f0eb31afc27524615c11566390d17792732fba3e
Kepler Boyce <keplerboy@gmail.com>
2023-10-14 19:46:41 -0400

    small css fixes

--- app/[location]/Checkbox.tsx
+++ app/[location]/Checkbox.tsx
@@ -6,7 +6,7 @@ export default function Checkbox(props: {
     const { label, on, setOn } = props;
 
     return (
-        <button className="py-0.5 text-sm font-semibold flex gap-2 items-center w-full bg-slate-200/0 hover:bg-slate-200 transition duration-150" onClick={() => setOn(!on)}>
+        <button className="py-0.5 px-2 rounded-md text-sm font-semibold flex gap-2 items-center w-full bg-slate-300/0 hover:bg-slate-300 transition duration-150" onClick={() => setOn(!on)}>
             <div className={"border-2 border-white ring-2 ring-slate-500 w-4 h-4 transition duration-150 "
                     + (on ? "bg-emerald-300" : "bg-white")} />
             {label}
